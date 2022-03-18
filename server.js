bbeb7266b204bb8890e8093f5e88daa0e17a5622
Peter Gish <pgish97@gmail.com>
2022-03-17 23:05:54 -0700

    added final project section. built proposal page

--- server.js
+++ server.js
@@ -20,35 +20,3 @@ fastify.listen(3000, (err, address) => {
    if (err) throw err
    // Server is now listening on ${address}
 })
-
-
-// const express = require("express")
-// const path = require("path")
-
-// const app = express()
-
-// app.use(
-//   "/css",
-//   express.static(path.join(__dirname, "node_modules/bootstrap/dist/css"))
-// )
-// app.use(
-//   "/css",
-//   express.static(path.join(__dirname, "node_modules/bootswatch/dist"))
-// )
-// app.use(
-//   "/fonts",
-//   express.static(path.join(__dirname, "node_modules/font-awesome"))
-// )
-// app.use(
-//   "/js",
-//   express.static(path.join(__dirname, "node_modules/bootstrap/dist/js"))
-// )
-// app.use("/js", express.static(path.join(__dirname, "node_modules/jquery/dist")))
-
-// app.get("/", (req, res) => {
-//   res.sendFile(path.join(__dirname, "static/new_index.html"))
-// })
-
-// app.listen(3000, () => {
-//   console.log("Listening on port " + 3000)
-// })
